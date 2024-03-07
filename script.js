493de1855e271718c21c0c1a972efa28fcb6a54f
mohamed elhaissan <154015982+alphaXProg@users.noreply.github.com>
2024-03-07 11:45:26 +0100

    final

---
+++ script.js
@@ -0,0 +1,8 @@
+var button = document.getElementById('hearting');
+function transfere(){
+    if (button.style.color == 'black') {
+        button.style.color='red';
+        alert("Liked");
+
+    }
+}
